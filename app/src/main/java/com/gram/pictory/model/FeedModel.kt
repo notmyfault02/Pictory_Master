6a4e06b464f322743c37d870080313df50f5de9c
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-10-01 20:02:28 +0900

    Update FeedModel

--- app/src/main/java/com/gram/pictory/model/FeedModel.kt
+++ app/src/main/java/com/gram/pictory/model/FeedModel.kt
@@ -1,13 +1,25 @@
 package com.gram.pictory.model
 
-data class FeedModel (
-    var postCode: Int,
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+data class FeedModel(
+    @SerializedName("username")
+    @Expose
     var username: String,
+    @SerializedName("id")
+    @Expose
     var id: String,
-    var text: String,
+    @SerializedName("liker_id")
+    @Expose
+    var likerId: List<String>,
+    @SerializedName("imageName")
+    @Expose
     var imageName: String,
-    var likeCount: Int,
-    var likeCheck: Boolean,
-    var replyCount: Int,
-    var postText: String
+    @SerializedName("comments")
+    @Expose
+    var comments: List<FeedCommentModel>,
+    var imagePath:String,
+    var text:String?
 )
