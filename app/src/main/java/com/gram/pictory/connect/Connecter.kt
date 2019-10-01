a8937ad2a51fccc91b0ec989dc28c610b8f7af09
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-10-01 20:00:56 +0900

    Update Connecter

--- app/src/main/java/com/gram/pictory/connect/Connecter.kt
+++ app/src/main/java/com/gram/pictory/connect/Connecter.kt
@@ -6,19 +6,18 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-object Connecter {
+object Connecter{
     var retrofit: Retrofit
     var api: API
-
     init {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
         val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
         retrofit = Retrofit
             .Builder()
-            .baseUrl("http://10.156.145.132:1212/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("http://54.180.116.34/api/")
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
 
